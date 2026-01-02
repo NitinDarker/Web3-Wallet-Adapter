cf3cebfbe9d71fa27e398d9e60f3ad91d5b0e059
Nitin Sharma <nitinsharma9829547681@gmail.com>
2026-01-02 17:24:36 +0530

    fix: shows alert when wallet is not connected

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -5,24 +5,37 @@ import { useRef } from 'react'
 export default function Airdrop () {
   const wallet = useWallet()
   const { connection } = useConnection()
-  const publicKey = wallet.publicKey?.toString()
+  const publicKey = wallet.publicKey
   const airdropCount = useRef<HTMLInputElement>(null)
 
   async function sendAirdrop () {
-    if (!airdropCount.current) return
+    if (!publicKey) {
+      alert('Connect wallet first!')
+      return
+    }
+
+    if (!airdropCount.current) {
+      alert('Enter an amount first!')
+      return
+    }
+
     const sol = Number(airdropCount.current.value)
     const drop = await connection.requestAirdrop(
-      wallet.publicKey!,
+      publicKey,
       sol * LAMPORTS_PER_SOL
     )
     console.log(drop)
   }
 
   return (
-    <div className='flex flex-col justify-center items-center p-10 w-screen gap-3 trasition-all'>
+    <div className='flex flex-col justify-center items-center p-10 pb-0 w-screen gap-3 trasition-all'>
       <div>
         <p className='text-center'>Hi</p>
-        <p>Your Public Key: {publicKey}</p>
+        {publicKey !== null ? (
+          <p>Your Public Key: {publicKey?.toString()}</p>
+        ) : (
+          <p>Connect to a wallet first</p>
+        )}
       </div>
       <input
         className='border border-neutral-400 rounded-lg p-1 text-sm w-lg'
